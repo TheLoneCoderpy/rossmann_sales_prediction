a2ac3d8945dc7a5a01d34c17d19b9a37fe0eabb6
Pascal <pascal.peters13@gmail.com>
2024-04-07 21:27:47 +0200

    ADA und GradientBoost added

--- best_params.py
+++ best_params.py
@@ -6,6 +6,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor, VotingRegressor
+from sklearn.ensemble import GradientBoostingRegressor, AdaBoostRegressor
 from sklearn.svm import SVR
 from xgboost import XGBRegressor
 import json
@@ -45,15 +46,15 @@ def make(X_train, y_train):
         print("Got the best forest params", X_train.shape, y_train.shape)
         return clf.best_params_
 
-    def get_best_svm_params(X_train, y_train):
-        parameters = {"svr__kernel": ["rbf", "sigmoid"],
-                      "svr__degree": [4, 5],
-                      "svr__C": [1000, 1200]}
-        pipeline = make_pipeline(StandardScaler(), SVR())
-        clf = GridSearchCV(pipeline, parameters, verbose=3, n_jobs=-1)
-        clf.fit(X_train, y_train)
-        print("Got the best svm params", X_train.shape, y_train.shape)
-        return clf.best_params_
+    #def get_best_svm_params(X_train, y_train):
+    #     parameters = {"svr__kernel": ["rbf", "sigmoid"],
+    #                   "svr__degree": [4, 5],
+    #                   "svr__C": [1000, 1200]}
+    #     pipeline = make_pipeline(StandardScaler(), SVR())
+    #     clf = GridSearchCV(pipeline, parameters, verbose=3, n_jobs=-1)
+    #     clf.fit(X_train, y_train)
+    #     print("Got the best svm params", X_train.shape, y_train.shape)
+    #     return clf.best_params_
 
     #def get_best_lin_reg_params(X_train, y_train):
     #    parameters = {"linearregression__fit_intercept": [True, False]}
@@ -72,6 +73,25 @@ def make(X_train, y_train):
         clf.fit(X_train, y_train)
         print("Got the best xg params", X_train.shape, y_train.shape)
         return clf.best_params_
+    
+    def get_best_adaboost_params(X_train, y_train):
+        parameters = {"adaboostregressor__n_estimators": [50, 100, 200],
+                    "adaboostregressor__learning_rate": [0.01, 0.1, 1]}
+        pipeline = make_pipeline(StandardScaler(), AdaBoostRegressor())
+        clf = GridSearchCV(pipeline, parameters, verbose=3, n_jobs=-1)
+        clf.fit(X_train, y_train)
+        print("Got the best adaboost params", X_train.shape, y_train.shape)
+        return clf.best_params_
+
+    def get_best_gradientboost_params(X_train, y_train):
+        parameters = {"gradientboostingregressor__n_estimators": [100, 200, 400],
+                    "gradientboostingregressor__learning_rate": [0.01, 0.1, 1],
+                    "gradientboostingregressor__max_depth": [3, 5, 7]}
+        pipeline = make_pipeline(StandardScaler(), GradientBoostingRegressor())
+        clf = GridSearchCV(pipeline, parameters, verbose=3, n_jobs=-1)
+        clf.fit(X_train, y_train)
+        print("Got the best gradientboost params", X_train.shape, y_train.shape)
+        return clf.best_params_
 
     knn_params = get_best_knn_params(X_train, y_train)
     tree_params = get_best_tree_params(X_train, y_train)
@@ -79,13 +99,18 @@ def make(X_train, y_train):
     svm_params = get_best_svm_params(X_train, y_train)
     #lin_reg_params = get_best_lin_reg_params(X_train, y_train)
     xg_params = get_best_xg_params(X_train, y_train)
+    adaboost_params = get_best_adaboost_params(X_train, y_train)
+    gradientboost_params = get_best_gradientboost_params(X_train, y_train)
+
 
     full_params = {"knn": knn_params, 
                    "tree": tree_params, 
                    "forest": forest_params, 
                    "svm": svm_params, 
-                   #"lin_reg": lin_reg_params, 
-                   "xg": xg_params}
+                   "lin_reg": lin_reg_params, 
+                   "xg": xg_params,
+                   "adaboost": adaboost_params,
+                   "gradientboost": gradientboost_params}
 
     with open("best_params.json", "w") as file:
         json.dump(full_params, file)
